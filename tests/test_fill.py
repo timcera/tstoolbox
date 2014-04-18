acd3e3afaf34cac2e3432997505b375b2ffaa626
Tim Cera <tim@cerazone.net>
2014-04-17 21:15:09 -0400

    Fixed date_slice to correctly handle start_date and end_date. Fixed fill tests to have the correct column names. Start of fill_by_correlation function.

--- tests/test_fill.py
+++ tests/test_fill.py
@@ -38,48 +38,48 @@ class TestFill(TestCase):
     def setUp(self):
         dindex = pd.date_range('2011-01-01T00:00:00', periods=26, freq='H')
         self.ats = pd.np.ones((26))*2
-        self.ats = pd.DataFrame(self.ats, index=dindex, columns=['Value_fill'])
+        self.ats = pd.DataFrame(self.ats, index=dindex, columns=['Value_with_missing_fill'])
         self.ats.index.name = 'Datetime'
 
         self.ats_cli = capture(tsutils._printiso, self.ats)
 
         self.ffill_compare = self.ats.copy()
-        self.ffill_compare['Value_fill']['2011-01-01T09:00:00':'2011-01-01T12:00:00'] = 3
-        self.ffill_compare['Value_fill']['2011-01-01T13:00:00'] = 9
+        self.ffill_compare['Value_with_missing_fill']['2011-01-01T09:00:00':'2011-01-01T12:00:00'] = 3
+        self.ffill_compare['Value_with_missing_fill']['2011-01-01T13:00:00'] = 9
 
         self.ffill_compare_cli = capture(tsutils._printiso, self.ffill_compare)
 
         self.bfill_compare = self.ats.copy()
-        self.bfill_compare['Value_fill']['2011-01-01T09:00:00'] = 3
-        self.bfill_compare['Value_fill']['2011-01-01T10:00:00':'2011-01-01T13:00:00'] = 9
+        self.bfill_compare['Value_with_missing_fill']['2011-01-01T09:00:00'] = 3
+        self.bfill_compare['Value_with_missing_fill']['2011-01-01T10:00:00':'2011-01-01T13:00:00'] = 9
 
         self.bfill_compare_cli = capture(tsutils._printiso, self.bfill_compare)
 
         self.linear_compare = self.ats.copy()
-        self.linear_compare['Value_fill']['2011-01-01T09:00:00'] = 3.0
-        self.linear_compare['Value_fill']['2011-01-01T10:00:00'] = 4.5
-        self.linear_compare['Value_fill']['2011-01-01T11:00:00'] = 6.0
-        self.linear_compare['Value_fill']['2011-01-01T12:00:00'] = 7.5
-        self.linear_compare['Value_fill']['2011-01-01T13:00:00'] = 9.0
+        self.linear_compare['Value_with_missing_fill']['2011-01-01T09:00:00'] = 3.0
+        self.linear_compare['Value_with_missing_fill']['2011-01-01T10:00:00'] = 4.5
+        self.linear_compare['Value_with_missing_fill']['2011-01-01T11:00:00'] = 6.0
+        self.linear_compare['Value_with_missing_fill']['2011-01-01T12:00:00'] = 7.5
+        self.linear_compare['Value_with_missing_fill']['2011-01-01T13:00:00'] = 9.0
 
         self.linear_compare_cli = capture(tsutils._printiso, self.linear_compare)
 
         self.nearest_compare = self.ats.copy()
-        self.nearest_compare['Value_fill']['2011-01-01T09:00:00':'2011-01-01T11:00:00'] = 3.0
-        self.nearest_compare['Value_fill']['2011-01-01T12:00:00':'2011-01-01T13:00:00'] = 9.0
+        self.nearest_compare['Value_with_missing_fill']['2011-01-01T09:00:00':'2011-01-01T11:00:00'] = 3.0
+        self.nearest_compare['Value_with_missing_fill']['2011-01-01T12:00:00':'2011-01-01T13:00:00'] = 9.0
 
         self.nearest_compare_cli = capture(tsutils._printiso, self.nearest_compare)
 
         self.mean_compare = self.ats.copy()
-        self.mean_compare['Value_fill']['2011-01-01T01:00:00'] = 2.4210526315789473
-        self.mean_compare['Value_fill']['2011-01-01T09:00:00'] = 3.0
-        self.mean_compare['Value_fill']['2011-01-01T10:00:00'] = 2.4210526315789473
-        self.mean_compare['Value_fill']['2011-01-01T11:00:00'] = 2.4210526315789473
-        self.mean_compare['Value_fill']['2011-01-01T12:00:00'] = 2.4210526315789473
-        self.mean_compare['Value_fill']['2011-01-01T13:00:00'] = 9.0
-        self.mean_compare['Value_fill']['2011-01-01T16:00:00'] = 2.4210526315789473
-        self.mean_compare['Value_fill']['2011-01-01T22:00:00'] = 2.4210526315789473
-        self.mean_compare['Value_fill']['2011-01-01T23:00:00'] = 2.4210526315789473
+        self.mean_compare['Value_with_missing_fill']['2011-01-01T01:00:00'] = 2.4210526315789473
+        self.mean_compare['Value_with_missing_fill']['2011-01-01T09:00:00'] = 3.0
+        self.mean_compare['Value_with_missing_fill']['2011-01-01T10:00:00'] = 2.4210526315789473
+        self.mean_compare['Value_with_missing_fill']['2011-01-01T11:00:00'] = 2.4210526315789473
+        self.mean_compare['Value_with_missing_fill']['2011-01-01T12:00:00'] = 2.4210526315789473
+        self.mean_compare['Value_with_missing_fill']['2011-01-01T13:00:00'] = 9.0
+        self.mean_compare['Value_with_missing_fill']['2011-01-01T16:00:00'] = 2.4210526315789473
+        self.mean_compare['Value_with_missing_fill']['2011-01-01T22:00:00'] = 2.4210526315789473
+        self.mean_compare['Value_with_missing_fill']['2011-01-01T23:00:00'] = 2.4210526315789473
 
         self.mean_compare_cli = capture(tsutils._printiso, self.mean_compare)
 
