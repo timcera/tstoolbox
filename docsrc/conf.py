fdba6d24b6b44b7d89aae5ad31da3ca78295f2aa
Tim Cera <tim@cerazone.net>
2016-09-15 23:07:35 -0400

    Forgot that RTD uses sphinx.ext.mathjax

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -30,7 +30,7 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.intersphinx',
               'sphinx.ext.todo',
               'sphinx.ext.coverage',
-              'sphinx.ext.imgmath',
+              'sphinx.ext.mathjax',
               'sphinx.ext.ifconfig',
               'sphinx.ext.viewcode',
               'sphinxcontrib.programoutput']
