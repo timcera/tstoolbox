b6230a0ef5b4fe0d1d03dad66439572a4e126f41
Tim Cera <tim@cerazone.net>
2018-05-28 18:57:35 -0400

    FEAT: Forgot to commit 'gof.py'.

---
+++ tstoolbox/functions/gof.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+"""Collection of functions for the manipulation of time series."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import warnings
+
+import mando
+from mando.rst_text_formatter import RSTHelpFormatter
+
+from .. import tsutils
+
+warnings.filterwarnings('ignore')
+
+
+@mando.command(formatter_class=RSTHelpFormatter, doctype='numpy')
+@tsutils.doc(tsutils.docstrings)
+def gof(input_ts='-',
+        stats='all',
+        columns=None,
+        start_date=None,
+        end_date=None,
+        round_index=None):
+    """Will calculate goodness of fit statistics between two time-series.
+
+    The first time series must be the observed, the second the predicted
+    series.  You can only give two time-series.
+
+    Parameters
+    ----------
+    {input_ts}
+    {columns}
+    {start_date}
+    {end_date}
+    {round_index}
+
+    """
+    if stats == 'all':
+        stats = ['bias', 'rmsd', 'crmsd', 'corrcoef', 'ss']
+    else:
+        try:
+            stats = stats.split(',')
+        except AttributeError:
+            pass
+
+    # Use dropna='all' to make sure that both have the same missing data.
+    tsd = tsutils.common_kwds(tsutils.read_iso_ts(input_ts),
+                              start_date=start_date,
+                              end_date=end_date,
+                              pick=columns,
+                              round_index=round_index,
+                              dropna='all')
+    if len(tsd.columns) != 2:
+        raise ValueError("""
+*
+*   The gof algorithms work with two time-series only.  You gave {0}.
+*
+""".format(len(tsd.columns)))
+
+    import skill_metrics as sm
+    import pandas as pd
+
+    statval = []
+
+    ref = tsd.iloc[:, 0].values
+    pred = tsd.iloc[:, 1].values
+
+    if 'bias' in stats:
+        statval.append(['Bias', sm.bias(pred, ref)])
+
+    if 'rmsd' in stats:
+        statval.append(['rmsd', sm.rmsd(pred, ref)])
+
+    if 'crmsd' in stats:
+        statval.append(['crmsd', sm.centered_rms_dev(pred, ref)])
+
+    if 'corrcoef' in stats:
+        statval.append(['corrcoef', pd.np.corrcoef(pred, ref)[0, 1]])
+
+    if 'ss' in stats:
+        statval.append(['Skill score (Murphy)',
+                        sm.skill_score_murphy(pred, ref)])
+
+    return tsutils.printiso(statval,
+                            tablefmt='plain',
+                            headers=['Statistic', 'Value'])
