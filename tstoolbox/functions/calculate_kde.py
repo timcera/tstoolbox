bd64c3a29023de7966f6894307d498cad2da3f57
Tim Cera <tim@cerazone.net>
2018-10-09 17:00:22 -0400

    FEAT: Added "calculate_kde" that returns a Kernel Densitiy Estimate probablity.

---
+++ tstoolbox/functions/calculate_kde.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+"""Collection of functions for the manipulation of time series."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import warnings
+
+import mando
+from mando.rst_text_formatter import RSTHelpFormatter
+
+import pandas as pd
+
+from .. import tsutils
+
+warnings.filterwarnings('ignore')
+
+
+@mando.command(formatter_class=RSTHelpFormatter, doctype='numpy')
+@tsutils.doc(tsutils.docstrings)
+def calculate_kde(ascending=True,
+                  evaluate=False,
+                  input_ts='-',
+                  columns=None,
+                  start_date=None,
+                  end_date=None,
+                  clean=False,
+                  skiprows=None,
+                  index_type='datetime',
+                  names=None):
+    """Return the kernel density estimation (KDE) curve.
+
+    Returns a time-series or the KDE curve depending on the `evaluate` keyword.
+
+    Parameters
+    ----------
+    ascending : bool
+        Sort order defaults to True.
+    evaluate : bool
+        Whether or not to return a time-series of KDE density values or the KDE
+        curve.  Defaults to False, which would return the KDE curve.
+    {input_ts}
+    {columns}
+    {start_date}
+    {end_date}
+    {skiprows}
+    {index_type}
+    {names}
+    {clean}
+
+    """
+    tsd = tsutils.common_kwds(tsutils.read_iso_ts(input_ts,
+                                                  skiprows=skiprows,
+                                                  names=names,
+                                                  index_type=index_type),
+                              start_date=start_date,
+                              end_date=end_date,
+                              pick=columns,
+                              clean=clean)
+
+    if len(tsd.columns) > 1:
+        raise ValueError("""
+*
+*   Right now "calculate_kde" only support one time-series at a time.
+*   You gave {0}.
+*
+""".format(tsd.columns))
+
+    from scipy.stats import gaussian_kde
+
+    tmptsd = tsd.dropna()
+    ndf = tmptsd.sort_values(tmptsd.columns[0], ascending=ascending)
+    gkde = gaussian_kde(ndf.iloc[:, 0])
+
+    if evaluate is True:
+        y = gkde.evaluate(tmptsd.iloc[:, 0])
+        ndf = pd.DataFrame(y, index=tmptsd.index)
+    else:
+        y = gkde.evaluate(ndf.iloc[:, 0])
+        ndf = pd.DataFrame(y)
+
+    return tsutils.printiso(ndf,
+                            showindex='always')
--- tstoolbox/tstoolbox.py
+++ tstoolbox/tstoolbox.py
@@ -13,41 +13,42 @@ import mando
 
 from . import tsutils
 
-from .functions.plot import plot
+from .functions.accumulate import accumulate
+from .functions.add_trend import add_trend
+from .functions.aggregate import aggregate
+from .functions.calculate_fdc import calculate_fdc
+from .functions.calculate_kde import calculate_kde
+from .functions.clip import clip
+from .functions.convert import convert
+from .functions.convert_index import convert_index
+from .functions.convert_index_to_julian import convert_index_to_julian
+from .functions.converttz import converttz
 from .functions.createts import createts
-from .functions.filter import filter
-from .functions.read import read
+from .functions.date_offset import date_offset
 from .functions.date_slice import date_slice
 from .functions.describe import describe
-from .functions.peak_detection import peak_detection
-from .functions.convert import convert
+from .functions.dtw import dtw
 from .functions.equation import equation
-from .functions.pick import pick
-from .functions.stdtozrxp import stdtozrxp
-from .functions.tstopickle import tstopickle
-from .functions.accumulate import accumulate
 from .functions.ewm_window import ewm_window
 from .functions.expanding_window import expanding_window
-from .functions.rolling_window import rolling_window
-from .functions.aggregate import aggregate
-from .functions.replace import replace
-from .functions.clip import clip
-from .functions.add_trend import add_trend
-from .functions.remove_trend import remove_trend
-from .functions.calculate_fdc import calculate_fdc
-from .functions.stack import stack
-from .functions.unstack import unstack
 from .functions.fill import fill
+from .functions.filter import filter
 from .functions.gof import gof
-from .functions.dtw import dtw
-from .functions.pca import pca
 from .functions.normalization import normalization
-from .functions.converttz import converttz
-from .functions.convert_index_to_julian import convert_index_to_julian
-from .functions.convert_index import convert_index
+from .functions.pca import pca
 from .functions.pct_change import pct_change
+from .functions.peak_detection import peak_detection
+from .functions.pick import pick
+from .functions.plot import plot
 from .functions.rank import rank
-from .functions.date_offset import date_offset
+from .functions.read import read
+from .functions.remove_trend import remove_trend
+from .functions.replace import replace
+from .functions.rolling_window import rolling_window
+from .functions.stack import stack
+from .functions.stdtozrxp import stdtozrxp
+from .functions.tstopickle import tstopickle
+from .functions.unstack import unstack
 
 warnings.filterwarnings('ignore')
 
