fc4018f86d878a806ad0dd07f960eb00b5676781
Tim Cera <tim@cerazone.net>
2018-12-29 01:38:26 -0500

    TESTS: Wrong float type in test_replace.py, failin' on Travis

--- tests/test_replace.py
+++ tests/test_replace.py
@@ -13,7 +13,7 @@ from . import capture
 class TestReplace(TestCase):
     def setUp(self):
         dindex = pd.date_range('2011-01-01T00:00:00', periods=26, freq='H')
-        self.ats = pd.np.ones((26)).astype('float64')
+        self.ats = pd.np.ones((26)).astype('float32')
         self.ats = pd.DataFrame(self.ats,
                                 index=dindex,
                                 columns=['Value_with_missing_replace'])
