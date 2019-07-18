67e59d7a9c08d561733589de15d5270a695bf4f6
Tim Cera <tim@cerazone.net>
2019-07-18 06:47:40 -0400

    TESTS: Limited tests to match what is available from base project.

--- tests/test_gof.py
+++ tests/test_gof.py
@@ -11,15 +11,11 @@ class Testgof(TestCase):
         self.df = tstoolbox.read(["tests/data_sunspot.csv", "tests/data_sunspot.csv"])
         self.gof = [
             ["Bias", 0.0],
-            ["Percent bias", 0.0],
-            ["Absolute percent bias", 0.0],
             ["Root-mean-square Deviation (RMSD)", 0.0],
             ["Centered RMSD (CRMSD)", 0.0],
             ["Correlation coefficient (r)", 1.0],
             ["Skill score (Murphy)", 1.0],
             ["Nash-Sutcliffe Efficiency", 1.0],
-            ["Kling-Gupta Efficiency", 1.0],
-            ["Index of agreement", 1.0],
             ["Common count observed and simulated", 1672],
             ["Count of NaNs", "", 0, 0],
         ]
@@ -30,15 +26,11 @@ class Testgof(TestCase):
             input_ts=self.df,
             stats=[
                 "bias",
-                "pc_bias",
-                "apc_bias",
                 "rmsd",
                 "crmsd",
                 "corrcoef",
                 "murphyss",
                 "nse",
-                "kge",
-                "index_agreement",
                 "brierss",
             ],
         )
