0e903c14cbb9ec56cec55284910e2e762b6b8c5b
Tim Cera <tim@cerazone.net>
2024-02-24 22:36:48 -0500

    test: small cleanup of test_convert_units.py

--- tests/test_convert_units.py
+++ tests/test_convert_units.py
@@ -8,7 +8,7 @@ from tstoolbox import tstoolbox
 
 class TestConvertUnits(TestCase):
     @staticmethod
-    def test_convert_units():
+    def test_convert_units_in():
         a = tstoolbox.read("tests/data_gainesville_daily_precip.csv", target_units="in")
         b = tstoolbox.equation(
             "x1/25.4", input_ts="tests/data_gainesville_daily_precip.csv"
@@ -16,6 +16,8 @@ class TestConvertUnits(TestCase):
         b.columns = ["ADaymet-prcp:in"]
         assert_frame_equal(a, b, check_dtype=False)
 
+    @staticmethod
+    def test_convert_units_km():
         a = tstoolbox.read("tests/data_gainesville_daily_precip.csv", target_units="km")
         b = tstoolbox.equation(
             "x1/(1000*1000)", input_ts="tests/data_gainesville_daily_precip.csv"
@@ -23,6 +25,8 @@ class TestConvertUnits(TestCase):
         b.columns = ["ADaymet-prcp:km"]
         assert_frame_equal(a, b, check_dtype=False)
 
+    @staticmethod
+    def test_convert_units_cfs_error_01():
         with pytest.raises(ValueError) as e_info:
             _ = tstoolbox.read(
                 "tests/data_gainesville_daily_precip.csv", source_units="ft3/s"
@@ -31,6 +35,8 @@ class TestConvertUnits(TestCase):
             e_info.value
         )
 
+    @staticmethod
+    def test_convert_units_cfs_error_02():
         with pytest.raises(ValueError) as e_info:
             _ = tstoolbox.read(
                 "tests/data_gainesville_daily_precip.csv", target_units="ft3/s"
