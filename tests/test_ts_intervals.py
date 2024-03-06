12cb2e57fd2c9b349da8539250c23ac793c9b013
Tim Cera <tim@cerazone.net>
2024-03-06 00:58:42 -0500

    test: another try to fix test_ts_intervals

--- tests/test_ts_intervals.py
+++ tests/test_ts_intervals.py
@@ -156,7 +156,7 @@ class TestAddTrend(TestCase):
             testcode = matches.get(testcode, testcode)
             logging.warning(f"{testcode} {inferred_code}")
             try:
-                if inferred_code == "1YE":
+                if inferred_code.split("-")[0] == "1YE":
                     testcode = "1YE"
                 self.assertEqual(testcode, inferred_code.split("-")[0])
             except AssertionError:
